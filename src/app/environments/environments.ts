4d8f3987fd60521a175f28b19f9a37efdd082475
Bloddy20Moon <118792974+Bloddy20Moon@users.noreply.github.com>
2024-05-13 18:54:07 -0500

    SistemaCompletoFront

---
+++ src/app/environments/environments.ts
@@ -0,0 +1,36 @@
+export const environment = {
+    production: false,
+    BACKEND_URL: 'http://localhost:3000/' + 'api/v1/',
+}
+
+
+export const dataSidebar = {
+
+}
+
+export const vTexto = {
+    login: {
+        tittle: 'Iniciar Sesión',
+        user: 'Usuario',
+        password: 'Contraseña',
+        buttonLogin: 'Ingresar'
+    },
+    sidebar: {
+        tittle: 'FOOTLOOSE',
+        buttonSinup: 'Cerrar Sesión'
+    },
+    header: {
+        userName: 'Jordan Chuquimajo',
+        userProfile: "Perfil",
+        logout: "Cerrar Sesión"
+
+
+    }
+}
+
+export const vImage = {
+    sidebar: {
+        normal: '../../assets/img/icon/favicon_footloose_.ico',
+        extend: '../../assets/img/icon/logo.ico'
+    }
+}
